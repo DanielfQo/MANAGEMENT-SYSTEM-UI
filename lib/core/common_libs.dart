eef2197bd474734988a52dd69edb25cee2e1ff31
DanielfQo <danielfrank12122004@gmail.com>
2026-02-18 09:13:06 -0500

    feat: create common_libs.dart for core dependencies and exports

---
+++ lib/core/common_libs.dart
@@ -0,0 +1,12 @@
+// lib/core/common_libs.dart
+
+
+import 'package:flutter/material.dart';
+// Dependencias de terceros
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:dio/dio.dart';
+export 'package:go_router/go_router.dart';
+
+// Tus archivos de core que usas en todos lados
+export 'network/api_client.dart';
+export 'constants/constants.dart';
