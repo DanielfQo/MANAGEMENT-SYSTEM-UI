06bd8c9116e24d329374416349823c7b2480f70e
GiomarMC <gmunozcu@unsa.edu.pe>
2026-02-27 22:36:01 -0500

    refactor(core): mejorar configuración y manejo de api_client
    - Se ajusta configuración base de requests
    - Se mejora manejo de headers de autenticación
    - Se prepara cliente para nuevos módulos

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,13 +1,30 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod/riverpod.dart';
+import '../utils/storage_service.dart';
 
 // Este provider crea una única instancia de Dio para toda la app
 final dioProvider = Provider<Dio>((ref) {
-  return Dio(
+  final dio = Dio(
     BaseOptions(
-      baseUrl: 'http://127.0.0.1:8000/api/',
+      baseUrl: 'http://10.0.2.2:8000/api/',
       connectTimeout: const Duration(seconds: 5),
     ),
   );
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final token = await StorageService.getToken();
+
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+
+        return handler.next(options);
+      },
+    ),
+  );
+
+  return dio;
 });
